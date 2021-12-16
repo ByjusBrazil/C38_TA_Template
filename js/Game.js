f81584cbb164e4d8fd804164ec462ef80c9f531a
ByjusBrazil <91496228+ByjusBrazil@users.noreply.github.com>
2021-12-16 11:59:22 -0300

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,54 @@
+class Game {
+  constructor() {}
+
+  getState(){
+
+    //defina as funções da classe
+
+
+}
+
+  
+
+  update(state){
+    
+    //defina as funções da classe
+
+  }
+
+  start() {
+    
+    // crie uma instância de novo jogador
+   
+    // inicie a variável playerCount
+    
+
+    form = new Form();
+    form.display();
+
+    car1 = createSprite (width/2 - 50, height - 100);
+    car1.addImage("car1",car1_img);
+    car1.scale = 0.07;
+
+    // siga o exemplo acima para criar o sprite de car2
+
+
+    // atribua os objetos ao vetor cars
+    
+
+  }
+
+  handleElements(){
+    // adicione os estilos à imagem do título.
+  }
+
+  play () {
+
+    //chame a função para esconder os elementos
+    this.handleElements();
+
+    //chame a função para criar os sprites
+    drawSprites();
+  
+  }
+}
